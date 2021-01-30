b7ad81e43ba1eb421e6f7a312b3e4af887adb757
Phillip O'Reggio <pnoreggio@gmail.com>
2021-01-29 20:55:00 -0500

    Draw the map

--- src/main.rs
+++ src/main.rs
@@ -16,22 +16,87 @@ use world::map;
 use entities::{component, factory};
 use component::*;
 use systems::*;
-use utility::text_canvas::TextCanvas;
+use utility::text_canvas::{TextCanvas, CanvasSymbol};
 use game::Command;
+
+use std::rc::Rc;
 
 use std::thread;
 use std::time::Duration;
 
+use std::io::{self, Write};
+use tui::Terminal;
+use tui::backend::CrosstermBackend;
+use tui::widgets::{Widget, Block, Borders, Paragraph};
+use tui::layout::{Layout, Constraint, Direction};
+
+use crossterm::terminal::{Clear, ClearType};
+
+extern crate nalgebra;
+use nalgebra::Vector2;
+
 fn main() {
-    test();
+    test().unwrap();
 }
 
-fn test() {
+fn test() -> Result<(), io::Error> {
+    let mut stdout = io::stdout();
+
+    // clear the screen
+    write!(stdout, "{}", Clear(ClearType::All))?;
+
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
     let map = map::test_room();
+    let mut canvas = TextCanvas::for_map(&map);
+    let character = CanvasSymbol {
+        character: '!',
+        foreground: Color::Red,
+        background: Color::Blue,
+        modifiers: vec!(),
+    };
 
-    let _dont_care_color = Color::Rgb(0, 0, 0); // color i don't think will be seen
+    canvas.set_symbol(Vector2::new(3, 3), character);
 
-    // let map_view = MapView::new(&map, rx);
+    let canvas_ptr = Rc::new(canvas);
+
+    for _ in 0..10 {
+        let canvas_ptr_clone = Rc::clone(&canvas_ptr);
+
+        terminal.draw(move |f| {
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints(
+                    [
+                        Constraint::Percentage(10),
+                        Constraint::Percentage(80),
+                        Constraint::Percentage(10)
+                    ].as_ref())
+                .split(f.size());
+
+            let block = Block::default()
+                .title("Block")
+                .borders(Borders::ALL);
+
+            f.render_widget(block, chunks[0]);
+
+            let map_text = (*canvas_ptr_clone).as_styled_text();
+            let map_display = Paragraph::new(map_text)
+                .block(
+                    Block::default()
+                        .title("map!")
+                        .borders(Borders::ALL)
+                );
+
+            f.render_widget(map_display, chunks[1]);
+        })?;
+
+        thread::sleep(Duration::from_millis(1000));
+    }
+
+    Ok(())
 }
 
 fn init_game() {
